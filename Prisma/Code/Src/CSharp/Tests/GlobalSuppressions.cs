7b9242cea29cb908ba914dee0d9b62b7b1fbd44a
hebelmx <hebelmx@gmail.com>
2025-11-18 14:50:06 -0600

    Deleted obsolet testing

--- Prisma/Code/Src/CSharp/Tests/GlobalSuppressions.cs
+++
@@ -1,6 +0,0 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
-
-[assembly: SuppressMessage("Style", "IDE0058:Expression value is never used", Justification = "<Pending>", Scope = "member", Target = "~M:ExxerCube.Prisma.Tests.Application.Services.ConfigurationValidationServiceTests.ValidateConfiguration_ValidConfig_ReturnsSuccess")]
